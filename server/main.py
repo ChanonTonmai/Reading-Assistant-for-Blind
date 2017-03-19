abef6d2b7af9ce5ad3b173ebfe68eb3d6f00d091
Chanon Khongprasongsiri <Chanon.khong@mail.kmutt.ac.th>
2017-03-19 14:56:07 +0700

    Update main.py

--- server/main.py
+++ server/main.py
@@ -1,1 +1,17 @@
+from flask import Flask, render_template
+from flask_script import Manager
+app = Flask(__name__, template_folder='templates')
+
+app.debug = True
+app.secret_key = 'development key'
+app.config['TEMPLATES_AUTO_RELOAD'] = True
+
+manager = Manager(app)
+
+@app.route('/')
+def index():
+    return render_template('index.txt')
+
+if __name__ == '__main__':
+    manager.run()
 
